399ee73f463c921f96bfa0bb620bd027e6980ad9
Shennie Yao <shennie.yao@microsoft.com>
2023-03-16 11:27:06 -0700

    project load/unload

--- Nodejs/Product/Nodejs/Commands/MigrateToJspsCommand.cs
+++ Nodejs/Product/Nodejs/Commands/MigrateToJspsCommand.cs
@@ -19,18 +19,42 @@ namespace Microsoft.NodejsTools.Commands
 
         public override void DoCommand(object sender, EventArgs args)
         {
+            var vsSolution = NodejsPackage.Instance.GetService<SVsSolution, IVsSolution>();
+            var vsSolution4 = NodejsPackage.Instance.GetService<SVsSolution, IVsSolution4>();
+            var uiShell = NodejsPackage.Instance.GetService<SVsUIShell, IVsUIShell>();
+
             Array activeProjects = (Array)NodejsPackage.Instance.DTE.ActiveSolutionProjects;
             EnvDTE.Project project = (EnvDTE.Project)activeProjects.GetValue(0);
 
             string projectFilepath = project.FullName;
 
-            var nodeProject = (NodejsProjectNode) project.Object;
+            var nodeProject = (NodejsProjectNode)project.Object;
             string parentProjectDir = Path.GetDirectoryName(nodeProject.ProjectFolder);
 
-            ThreadHelper.JoinableTaskFactory.RunAsync(async delegate
-            {
-                MigrationLibrary.Migrate(projectFilepath, parentProjectDir);
-            });
+            project.Save();
+            NodejsPackage.Instance.DTE.Solution.Remove(project);
+
+            //ThreadHelper.JoinableTaskFactory.RunAsync(async delegate
+            //{
+            //    MigrationLibrary.Migrate(projectFilepath, parentProjectDir);
+            //});
+            var newProjectFilePath = MigrationLibrary.Migrate(projectFilepath, parentProjectDir);
+
+            NodejsPackage.Instance.DTE.Solution.AddFromFile(newProjectFilePath, false);
+
+            //Guid emptyGuid = Guid.Empty;
+            //Guid newProjectIdentifier = new Guid();
+            //int result = vsSolution.CreateProject(ref emptyGuid, newProjectFilePath, null, null, (uint)__VSCREATEPROJFLAGS.CPF_OPENFILE, ref newProjectIdentifier, out IntPtr projectPtr);
+            //if (result == 0)
+            //{
+            //    // yay! do nothing
+            //}
+            //else
+            //{
+            //    uiShell.ReportErrorInfo(result);
+            //    //NodejsPackage.Instance.DTE.Solution.AddFromFile(newProjectFilePath, false);
+            //}
+
         }
     }
 }
